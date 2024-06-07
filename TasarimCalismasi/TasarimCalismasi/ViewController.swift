6da1118ac22681a873b209e9a3b5b87a674f9e39
Muhammet BOZKURT <89414647+Mami0121@users.noreply.github.com>
2024-06-07 10:52:26 +0300

    IOS Tasarım Çalışması
    Playground üzerinde tasarım çalışması

---
+++ TasarimCalismasi/TasarimCalismasi/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  TasarimCalismasi
+//
+//  Created by Bozkurt on 24.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "Pizza"
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor(named: "anaRenk")
+        appearance.titleTextAttributes = [.foregroundColor : UIColor(named: "yaziRenk1")!,.font:UIFont(name: "Pacifico-Regular", size: 22)]
+        navigationController?.navigationBar.barStyle = .black
+        
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+
+        
+                
+    }
+
+}
+
